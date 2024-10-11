c6aebce28a4cc5ee77eff0eb3db068232d0f0d02
Stefan Ginev <choephix@gmail.com>
2024-10-11 09:10:39 +0300

    Update App component: enhance cleanup logic in useEffect to reset IDE state along with world data on unmount

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { useEffect } from 'react';
 import GameSandbox from './GameSandbox';
 import { populateSampleWorld } from './misc/sampleWorld';
 import { worldDataStateActions } from './stores/worldDataState';
+import { ideStateActions } from './stores/ideStore';
 
 import './globalActions';
-import './App.css'
-;
+import './App.css';
+
 function App() {
   useEffect(() => {
     populateSampleWorld();
 
-    return () => worldDataStateActions.clearWorld();
-  });
+    return () => {
+      worldDataStateActions.clearWorld();
+      ideStateActions.setSelectedEntityId(null);
+    };
+  }, []); // Added empty dependency array to run effect only once
 
   return <GameSandbox />;
 }
