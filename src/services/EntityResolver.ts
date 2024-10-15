25356a565b9f9807845891dc114d1250a09689d2
Stefan Ginev <choephix@gmail.com>
2024-10-15 22:13:41 +0300

    Refactor behavior resolvers in EntityResolver: simplify behavior retrieval and consolidate update/render logic for clarity

--- src/services/EntityResolver.ts
+++ src/services/EntityResolver.ts
@@ -1,33 +1,24 @@
-import {
-  StageEntityProps,
-  BehaviorProps,
-  ReadonlyDeep,
-} from "../types/data-types";
-import { BehaviorResolver, behaviorResolvers } from "../behaviors/behaviors";
+import { getBehaviorResolver } from '../behaviors/behaviors';
+import { BehaviorProps, ReadonlyDeep, StageEntityProps } from '../types/data-types';
 
 export const EntityResolver = {
   update: (entity: StageEntityProps, deltaTime: number, totalTime: number) => {
-    entity.behaviors.forEach((behavior) => {
-      const resolverKey = behavior.type as keyof typeof behaviorResolvers;
-      const resolver = behaviorResolvers[resolverKey] as BehaviorResolver<typeof behavior>;
+    entity.behaviors.forEach(behavior => {
+      const resolver = getBehaviorResolver(behavior);
       resolver?.update?.apply(behavior, [entity, deltaTime, totalTime]);
     });
   },
 
   render: (entity: ReadonlyDeep<StageEntityProps>) => {
     let content: React.ReactNode = null;
-    entity.behaviors.forEach((behavior) => {
-      const resolverKey = behavior.type as keyof typeof behaviorResolvers;
-      const resolver = behaviorResolvers[resolverKey] as BehaviorResolver<typeof behavior>;
+    entity.behaviors.forEach(behavior => {
+      const resolver = getBehaviorResolver(behavior);
       content = resolver?.render?.apply(behavior, [entity, content]);
     });
     return content;
   },
 
-  getBehavior: (
-    entity: StageEntityProps,
-    type: string
-  ): BehaviorProps | undefined => {
-    return entity.behaviors.find((b) => b.type === type);
+  getBehavior: (entity: StageEntityProps, type: string): BehaviorProps | undefined => {
+    return entity.behaviors.find(b => b.type === type);
   },
 };
