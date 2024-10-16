c9df3c3117efc59909e42466c6495613884b68f9
Stefan Ginev <choephix@gmail.com>
2024-10-16 17:15:31 +0300

    Refactor entityResolver: replace forEach with for...of for better readability and null-check on behaviors array

--- src/resolvers/entityResolver.ts
+++ src/resolvers/entityResolver.ts
@@ -3,18 +3,18 @@ import { getBuiltInBehaviorResolver } from "./getBuiltInBehaviorResolver";
 
 export const entityResolver = {
   onTick: (entity: StageEntityProps, deltaTime: number, totalTime: number) => {
-    entity.behaviors.forEach((behavior) => {
+    for (const behavior of entity.behaviors ?? []) {
       const resolver = getBuiltInBehaviorResolver(behavior);
       resolver?.onTick?.apply(behavior, [entity, deltaTime, totalTime]);
-    });
+    }
   },
 
   render: (entity: ReadonlyDeep<StageEntityProps>) => {
     let content: React.ReactNode = null;
-    entity.behaviors.forEach((behavior) => {
+    for (const behavior of entity.behaviors ?? []) {
       const resolver = getBuiltInBehaviorResolver(behavior);
       content = resolver?.render?.apply(behavior, [entity, content]);
-    });
+    }
     return content;
   },
 };
